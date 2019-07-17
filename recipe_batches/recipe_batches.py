d56b98db3eefa0353b9c82a78083b1db3f6b3809
Jarrad Miller <jm020801@gmail.com>
2019-07-17 15:39:55 -0600

    stock_prices and recipe_batches done

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,13 +2,24 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    batches_by_ingredient = []
+    for recipe_ingredient in recipe:
+        if recipe_ingredient in ingredients:
+            batches = ingredients[recipe_ingredient] / \
+                recipe[recipe_ingredient]
+            batches_by_ingredient.append(math.floor(batches))
+        else:
+            batches_by_ingredient.append(0)
+            break  # you only need to loop for as long as there's enough ingredients
+    return min(batches_by_ingredient)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+    # Change the entries of these dictionaries to test
+    # your implementation with different inputs
+    recipe = {'milk': 100, 'butter': 50, 'flour': 5}
+    ingredients = {'milk': 132, 'butter': 48, 'flour': 51}
+    print("{batches} batches can be made from the available ingredients: {ingredients}.".format(
+        batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,13 +3,28 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+    # this keeps track of how much we've spent
+    purchase_price = prices[0]
+    # this keeps track of the current max profit
+    profit = prices[1] - purchase_price
+
+    # start at index 1 because we've already done the first computation
+    for price in prices[1:]:
+        # returns the bigger number
+        profit = max(price - purchase_price, profit)
+        # returns the smaller number
+        purchase_price = min(purchase_price, price)
+
+    return profit
 
 
 if __name__ == '__main__':
-  # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    # This is just some code to accept inputs from the command line
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
